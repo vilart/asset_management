045047a9c2ff2130ad74de1b8c4423c1036c3732
vilart <artheilis@gmail.com>
2026-03-03 16:10:31 +0100

    feat: add audit log using django-simple-history

--- assets/models.py
+++ assets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from simple_history.models import HistoricalRecords
 
 # Create your models here.
 
@@ -39,6 +40,8 @@ class Asset(models.Model):
     ip_address = models.GenericIPAddressField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.NEW)
     additional_info = models.TextField(blank=True, null=True)
+
+    history = HistoricalRecords()
 
     def __str__(self):
         return f"{self.device_model} - {self.name} ({self.serial_number})"
